4cbbe464b239fd3b5c3ec87dff037ec427117f9a
Erica Inacia de Lima <ericainacia27@gmail.com>
2022-03-25 23:46:30 -0300

    Projeto-Whatslab

---
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     // <div className="App">
+//     //   <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Edit <code>src/App.js</code> and save to reload.
+//     //     </p>
+//     //     <a
+//     //       className="App-link"
+//     //       href="https://reactjs.org"
+//     //       target="_blank"
+//     //       rel="noopener noreferrer"
+//     //     >
+          
+//     //     </a>
+//     //   </header>
+//     // </div>
+
+
+//   );
+// }
+
+// export default App;
+class NomeDoUsuario extends React.Component {
+  render() {
+    return (
+      <div>
+        Olá, {this.props.name}!
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <HelloMessage name="Taylor" />,
+  document.getElementById('hello-example')
+);
