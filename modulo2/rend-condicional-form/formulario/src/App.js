9a73afcb8582b6f30461dc03cac728206791c58f
Erica Inacia de Lima <ericainacia27@gmail.com>
2022-03-29 00:23:37 -0300

    Formulário

---
+++ modulo2/rend-condicional-form/formulario/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+ return(
+   <h1> ETAPA 1 - DADOS GERAIS  </h1>
+   <form className="forms">
+   <label>
+     1. Qual o seu nome?
+    <input type="text" name="name" >
+     2. Qual sua idade?
+     <input type="text" name="name" >
+     3. Qual seu email?
+     <input type="text" name="name" >
+     2. Qual a sua escolaridade?
+     <input type="text" name="name" >
+   </label> 
+   <button type="button">Proxima etapa</button>
+   </form>
+    
+   
+ )
+}
+
+export default App;
