432231d77cc6e9dda68d2e3a5058e64483fafed8
Erica Inacia de Lima <ericainacia27@gmail.com>
2022-04-13 09:14:33 -0300

    Link Surge

--- modulo2/rend-condicional-form/formulario/src/App.js
+++ modulo2/rend-condicional-form/formulario/src/App.js
@@ -3,23 +3,27 @@ import './App.css';
 
 function App() {
  return(
-   <h1> ETAPA 1 - DADOS GERAIS  </h1>
+   <div>
+      <h1> ETAPA 1 - DADOS GERAIS  </h1>
    <form className="forms">
    <label>
      1. Qual o seu nome?
-    <input type="text" name="name" >
+    <input type="text" name="name" />
      2. Qual sua idade?
-     <input type="text" name="name" >
+     <input type="text" name="name"/>
      3. Qual seu email?
-     <input type="text" name="name" >
+     <input type="text" name="name"/>
      2. Qual a sua escolaridade?
-     <input type="text" name="name" >
+     <input type="text" name="name"/>
    </label> 
    <button type="button">Proxima etapa</button>
    </form>
+   </div>
+  
     
    
  )
 }
 
 export default App;
+
