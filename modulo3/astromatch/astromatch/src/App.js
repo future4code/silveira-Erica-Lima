1e21d0b1012b01070eb222edadc7102bdd331c80
Erica Inacia de Lima <ericainacia27@gmail.com>
2022-04-22 20:18:39 -0300

    AstroMatch

---
+++ modulo3/astromatch/astromatch/src/App.js
@@ -0,0 +1,19 @@
+import  React, {useEffect, useState} from "react"
+
+import styled from "styled-components"
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      oi
+    </div> 
+     )
+}
+
+export default App;
---
+++ modulo3/astromatch/astromatch/src/pages/Home/home.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from "react"
+import axios from "axios"
+
+
+
+export function Home(){
+     const [home, setHome] = useState({})
+    
+    
+    return(
+        <div>
+        <p>oi</p>
+    </div>
+
+    )
+    
+
+
+} 
