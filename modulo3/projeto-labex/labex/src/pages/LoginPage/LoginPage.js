29d0efabb53bbbe9a4caad514866a83b442c431c
Erica Inacia de Lima <ericainacia27@gmail.com>
2022-04-29 20:47:06 -0300

    Quinta atualização

--- modulo3/projeto-labex/labex/src/pages/LoginPage/LoginPage.js
+++ modulo3/projeto-labex/labex/src/pages/LoginPage/LoginPage.js
@@ -6,26 +6,63 @@ import { goToBackLogin } from '../../routes/coordinator';
 import { goToAdmin } from '../../routes/coordinator';
 
 
+
 export const LoginPage = () =>{
     const navigate = useNavigate()
-    const [email, setEmail] = useState("")
-    const [senha, setSenha] = useState("")
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const history = useNavigate()
+
+    const onChangeEmail = (e) =>{
+        setEmail(e.target.value)
+    };
+
+    const onChangePassword = (e) =>{
+        setPassword(e.target.value)
+    };
+
+    const onSubmitLogin = () => {
+        const body = {
+            email: email,
+            password: password
+        };
+
+        console.log(email, password)
+
+        axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labeX/erica-lima-silveira/login", body)
+        .then((response) => {
+            console.log(":)", response.data.token)
+            window.localStorage.setItem("token", response.data.token)
+            history.push("/tripDetails")
+        })
+        .cath((error) => {
+            console.log(":(", error.data)
+        })
+    }
    
     return(
         <div>
             <h1>Login</h1>
             <div>
             <label>
-          <input placeholder="E-mail" />
+          <input placeholder="email"
+          type="email"
+          value= {email}
+          onChange={onChangeEmail} />
         </label> <br></br>
         <label>
-          <input placeholder="Senha" />
+          <input placeholder="password"
+           type="password"
+           value= {password}
+           onChange={onChangePassword} />
         </label> <br></br>
             </div>
 
             <div>
-                <button onClick={()=>goToBackLogin(navigate)}>Voltar</button>
-                <button onClick={()=>goToAdmin(navigate)}>Entrar</button>
+                {/* <button onClick={ ()=>goToBackLogin(navigate) }>Voltar</button> */}
+                <button onClick={ ()=>goToBackLogin(navigate) }>Voltar</button>
+                {/* <button onClick={()=>goToAdmin(navigate)}>Entrar</button> */}
+                <button onClick={onSubmitLogin}>Entrar</button>
             </div>
         </div>
     )
--- modulo3/projeto-labex/labex/src/pages/TripDetailsPage/TripDetailsPage.js
+++ modulo3/projeto-labex/labex/src/pages/TripDetailsPage/TripDetailsPage.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React ,{useState,useEffect}from 'react'
 import { useNavigate } from "react-router-dom";
 import { goToBackTrip } from '../../routes/coordinator';
+import axios from "axios"
 
 
 export const TripDetailsPage =() => {
     const navigate = useNavigate()
+
+    const history = useNavigate()
+
+    useEffect(()=>{
+        const token = localStorage.getItem('token')
+        if (token === null){
+            console.log("Precisa fazer o login!!!!")
+            history.push("/login")
+        }
+
+    },[])
+
+    const token = localStorage.getItem('token')
+
+    useEffect (() =>{
+        const aluno = "erica-lima-silveira"
+     axios.get(`https://us-central1-labenu-apis.cloudfunctions.net/labeX/${aluno}/trip/:id`, {
+         headers: {
+             auth: token
+         }
+     })
+           
+        .then((response)=>{
+            console.log(response.data)
+        })
+      
+    .catch((error)=>{
+        console.log(error.response)
+    })
+      
+      }, [])
    
     return(
         <div>
