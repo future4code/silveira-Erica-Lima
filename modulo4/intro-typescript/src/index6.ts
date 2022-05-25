0edfa605347e30893b4810467997a447e2ca4192
Erica Inacia de Lima <ericainacia27@gmail.com>
2022-05-25 17:26:47 -0300

    introdrução-typescript

---
+++ modulo4/intro-typescript/src/index6.ts
@@ -0,0 +1,26 @@
+// function operation(num1: number, num2: number){
+//     switch () {
+//         case  "adicao" :
+//           console . log ( `Resultado da adiçaõ ${ num1 + num2 } ` )
+//           break ;
+//           case  "subtracao" :
+//             console . log ( `Resultado da subtração ${ num1 - num2 } ` )
+//             break ;
+//             case  "multiplicacao" :
+//                 console . log ( `Resultado da multiplicação ${ num1 - num2 } ` )
+//                 break;
+      
+//     if(num1 > num2 ||  num2 > num1){
+//         console.log("Maior número:");
+//     } else {
+
+//     }
+
+
+// }
+
+//Não deu tempo de terminar o desafio
+
+
+
+
