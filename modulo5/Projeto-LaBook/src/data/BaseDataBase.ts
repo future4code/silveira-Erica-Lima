0a565ada58974737de899becff39422f6fa4303d
Erica <ericainacia27@gmail.com>
2022-07-05 15:31:34 -0300

    Deletando follow.(Não estava fucionando)

---
+++ modulo5/Projeto-LaBook/src/data/BaseDataBase.ts
@@ -0,0 +1,17 @@
+import knex, { Knex } from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
+export const connection: Knex = knex({
+    client: "mysql",
+    connection: {
+       host: process.env.DB_HOST,
+       user: process.env.DB_USER,
+       password: process.env.DB_PASS,
+       database: process.env.DB_NAME,
+       port: 3306,
+       multipleStatements: true
+    }
+ })
+ 
+
